2c88407e8b1c1e9cb919b08224d16e858ff64be5
notcelis <isaac.celis.3c@gmail.com>
2020-08-06 17:28:33 -0500

    Sessions learned

--- project1/forms.py
+++ project1/forms.py
@@ -1,18 +1,26 @@
 from wtforms import Form
-from wtforms import StringField, TextField
-from wtforms.fields.html5 import EmailField
+from wtforms import StringField
+from wtforms import TextField
+from wtforms import PasswordField
+from wtforms import TextAreaField
+from wtforms import HiddenField
 from wtforms import validators
+from wtforms.fields.html5 import EmailField
+##from models import User
 
-class Miclaseform(Form):
-    username = StringField('username',
-                [
-                    validators.Required(message='Ingrese el usuario'),
-                    validators.length(min=4,max=25, message='Ingrese un usuario valido.')
-                ]
-                )
-    email = EmailField('Correo electronico',
-                [
-                    validators.Required(message='Ingrese el email'),
-                    validators.Email(message='Ingrese un email valido.')
-                ])
-    comment = TextField('Comentario')
+def length_honeypot(form, field):
+	if len(field.data) > 0:
+		raise validators.ValidationError('El campo debe estar vacio.')
+
+class CommentForm(Form):
+	comment = TextAreaField('Comentario')
+	honeypot = HiddenField('', [length_honeypot])
+
+class LoginForm(Form):
+	username = StringField('Username',
+				[ 
+					validators.Required(message = 'El username es requerido!.'),
+						validators.length(min=4, max=25, message='Ingrese un username valido!.'),
+				])
+	password = PasswordField('Password', [validators.Required(message='El password es requerido')])
+
--- project1/main.py
+++ project1/main.py
@@ -1,20 +1,50 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import make_response
+from flask import session
 
+from flask import url_for
+from flask import redirect
+
+from flask_wtf import CsrfProtect
 import forms
 
 app = Flask(__name__)
+app.secret_key = 'mi_palabra_secreta'
+csrf = CsrfProtect(app)
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/')
 def index():
-    comment_form = forms.Miclaseform(request.form)
-    if request.method == 'POST' and comment_form.validate():
-        print (comment_form.username.data)
-        print (comment_form.email.data)
-        print (comment_form.comment.data)
+    if 'username' in session:
+        username = session['username']
+        print (username)
 
-    return render_template('index.html', title="Isaac", form = comment_form)
+    title = 'Index'
+    return render_template('index.html', title = title)
+
+@app.route('/logout')
+def logout():
+    if 'username' in session:
+        session.pop('username')
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    login_form = forms.LoginForm(request.form)
+    if request.method == 'POST' and login_form.validate():
+        session['username'] = login_form.username.data
+    return render_template('login.html', form = login_form)
+
+@app.route('/cookie')
+def cookie():
+    reponse = make_response(render_template('cookie.html'))
+    reponse.set_cookie('mi_primera_cookie','Isaac')
+    return reponse
+
+@app.route('/comment',methods=['GET','POST'])
+def comment():
+    return render_template('comment.html')
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000)
